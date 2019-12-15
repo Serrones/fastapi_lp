1717a52ad9a9a562fe5c22e9868f29cca38d0902
Serrones <fabioserrones@gmail.com>
2019-12-15 16:53:35 -0300

    Add todo list view

--- main.py
+++ main.py
@@ -1,6 +1,22 @@
 from fastapi import FastAPI
+from typing import List, Dict, Any
 
 app = FastAPI()
+
+todo: List[Dict[str, Any]] = [    
+    {
+        'id': 1,
+        'title': 'FastApi',
+        'description': 'Explore FastApi application',
+        'status': 'doing'
+    },
+    {
+        'id': 2,
+        'title': 'AIOHTTP',
+        'description': 'Explore Async concepts with AIOHTTP',
+        'status': 'to do'
+    }
+]
 
 @app.get('/')
 def hello_world():
@@ -8,3 +24,10 @@ def hello_world():
     Root view, returns {'hello': 'world'}
     """
     return{'hello': 'world'}
+
+@app.get('/list')
+def list_todo():
+    """
+    View returns todo list
+    """
+    return todo   
--- tests.py
+++ tests.py
@@ -11,3 +11,11 @@ def test_main_status_code():
 def test_main_response_json():
     response = client.get('/')
     assert response.json() == {'hello': 'world'}
+
+def test_list_todo_status_code():
+    response = client.get('/list')
+    assert response.status_code == 200
+
+def test_list_todo_should_return_list():
+    response = client.get('/list')
+    assert type(response.json()) is list 
